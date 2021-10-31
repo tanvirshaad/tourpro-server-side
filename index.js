04e3bc5b38f768ddf3d77d8ac7528d0ed60a57d2
tanvirshaad <tanvirshaad@gmail.com>
2021-10-31 17:27:36 +0600

    order api added

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders);
         });
+        //Delete order APi
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        });
     } finally {
         // await client.close()
     }
