d8bc171d7e11995aef9e528aa802ab659a449817
tanvirshaad <tanvirshaad@gmail.com>
2021-10-31 20:51:39 +0600

    port edited

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
